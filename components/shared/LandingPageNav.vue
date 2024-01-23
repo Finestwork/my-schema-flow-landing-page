5d3fb690f12d44692ac1187d504d51606485b5c5
Finestwork <Younes.Espiritu@outlook.com>
2024-01-23 11:44:51 +0800

    Add prettier formatter

--- components/shared/LandingPageNav.vue
+++ components/shared/LandingPageNav.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import MySchemaFlowLogo from "./MySchemaFlowLogo.vue";
+import MySchemaFlowLogo from './MySchemaFlowLogo.vue';
 </script>
 <template>
-    <nav>
-        <MySchemaFlowLogo class="w-[35px] p-1 h-[35px]"/>
-        <div>
-            <NuxtLink>Features</NuxtLink>
-            <NuxtLink>Tech</NuxtLink>
-            <NuxtLink>FAQ</NuxtLink>
-            <NuxtLink>Contributors</NuxtLink>
-        </div>
-    </nav>
+  <nav>
+    <MySchemaFlowLogo class="h-[35px] w-[35px] p-1" />
+    <div>
+      <NuxtLink>Features</NuxtLink>
+      <NuxtLink>Tech</NuxtLink>
+      <NuxtLink>FAQ</NuxtLink>
+      <NuxtLink>Contributors</NuxtLink>
+    </div>
+  </nav>
 </template>
