650fbd3c64c91205651a517ee6abded4c5e106ee
Finestwork <Younes.Espiritu@outlook.com>
2024-01-23 15:43:14 +0800

    Add color states to navbar component

--- components/LandingPageNav.vue
+++ components/LandingPageNav.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="top-0 z-50 fixed left-0 w-full py-2">
     <div class="mx-auto flex w-[95%] items-center justify-between">
-        <span class="flex items-center">
+        <a href="/" class="flex items-center outline-none ring-0 focus-visible:ring-4 ring-cyan-500/50 transition-shadow">
           <Logo class="h-[35px] w-[35px] rounded-md p-1" />
           <span class="text-md font-mona ml-1.5 font-extrabold text-slate-900"
           >MySchemaFlow</span
           >
-        </span>
+        </a>
       <div>
         <VLandingPageLink class="mr-1.5" to="#features"
         >Home
--- components/VLandingPageLink.vue
+++ components/VLandingPageLink.vue
@@ -5,7 +5,7 @@ type TProps = {
 const props = defineProps<TProps>();
 </script>
 <template>
-  <NuxtLink class="font-bold text-slate-900 font-mona" :to="props.to">
+  <NuxtLink class="font-bold text-sm text-slate-900 font-mona hover:text-cyan-600 focus-visible:text-cyan-600" :to="props.to">
     <slot></slot>
   </NuxtLink>
 </template>
