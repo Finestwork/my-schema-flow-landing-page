51149d2a06552f20079e8dac737e2f93e0003dbd
Finestwork <Younes.Espiritu@outlook.com>
2024-01-23 23:11:41 +0800

    Retouch footer

--- components/LandingPageFooter.vue
+++ components/LandingPageFooter.vue
@@ -3,12 +3,12 @@
     <VSectionLayout>
       <div>
         <div class="flex items-center">
-          <Logo class="w-[40px] rounded-md p-2" />
+          <Logo class="w-[25px] rounded-md p-1.5" />
           <span class="ml-1 text-lg font-black text-white">MySchemaFlow</span>
         </div>
       </div>
       <hr class="my-4" />
-      <div class="flex justify-between font-medium text-white">
+      <div class="flex justify-between text-xs font-medium text-white">
         <p>© 2023 SchemaFlow. All rights reserved.</p>
         <p class="flex items-center justify-center text-center">
           Designed by
