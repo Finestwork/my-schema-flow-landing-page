f5fac5e222d2e0dcc1dfac3f0487dc4825f91541
Finestwork <Younes.Espiritu@outlook.com>
2024-01-24 21:58:21 +0800

    Add responsiveness to navigation drawer

--- components/LandingPageNavDrawerLink.vue
+++ components/LandingPageNavDrawerLink.vue
@@ -6,7 +6,7 @@ const props = defineProps<TProps>();
 </script>
 <template>
   <a
-    class="link relative z-10 px-2 py-2 text-[3rem] font-black before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:scale-y-0 before:bg-cyan-600 before:transition-transform hover:text-cyan-100 hover:before:scale-y-100"
+    class="link relative z-10 px-2 py-2 text-2xl font-black before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:scale-y-0 before:bg-cyan-600 before:transition-transform hover:text-cyan-100 hover:before:scale-y-100"
     :href="props.href"
   >
     <slot></slot>
