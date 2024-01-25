b9fbbafad1f3cac3d970bf318f5d012f901f6502
Finestwork <Younes.Espiritu@outlook.com>
2024-01-25 16:50:26 +0800

    Add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Welcome to MySchemaFlow! Your database schema visualizer',
+      meta: [
+        {
+          name: 'description',
+          content: 'MySchemaFlow is a database schema visualizer',
+        },
+      ],
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          type: 'image/x-icon',
+          href: '/icons/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          sizes: '16x16',
+          type: 'image/png',
+          href: '/icons/favicon-16x16.png',
+        },
+        {
+          rel: 'icon',
+          sizes: '32x32',
+          type: 'image/png',
+          href: '/icons/favicon-32x32.png',
+        },
+        {
+          rel: 'mask-icon',
+          color: '#686868',
+          href: '/icons/safari-pinned-tab.svg',
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   modules: ['@vue-macros/nuxt', '@nuxtjs/tailwindcss'],
   css: ['~/assets/css/font.css'],
-  macros: {},
 });
